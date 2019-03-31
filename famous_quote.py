51c7e1c8fd82eedf156da001c82c8f0cf0476345
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 21:15:06 -0500

    Add files via upload

---
+++ famous_quote.py
@@ -0,0 +1,6 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+famous_person = 'kobe bryant'
+print(famous_person.title() + ' once said, "If your goals dont scare you then they are not big enough."')
