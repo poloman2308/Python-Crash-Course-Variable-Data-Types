19a9ff3f233513de4c04831fb58a2c22bf1add4f
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 20:49:28 -0500

    Add files via upload

---
+++ simple_message.py
@@ -0,0 +1,11 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+# 2.1 Simple Message & 2.2 Simple Messages
+message = "Hello Python world!"
+print(message)
+
+message = "I am really enjoying coding with Python."
+print(message)
+
