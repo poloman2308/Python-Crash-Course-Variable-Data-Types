1b411aef8a39b22fd84b43386b8516bef05e6a01
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 22:30:43 -0500

    Add files via upload

---
+++ favorite_number.py
@@ -0,0 +1,9 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+favorite_number = 24
+favorite_player = 'kobe bryant'
+message = favorite_player.title() + " wears the number " + str(favorite_number) + " and is the GOAT!"
+
+print(message)
