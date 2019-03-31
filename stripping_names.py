022cc7b22ad0c63513531bd747fc282c07523f7a
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 21:28:02 -0500

    Add files via upload

---
+++ stripping_names.py
@@ -0,0 +1,18 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+name = "\tDerek Acevedo\n"
+
+print("Unmodified:")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
+
