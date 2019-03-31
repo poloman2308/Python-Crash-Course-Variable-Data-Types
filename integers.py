68141c2f64b6f8b8a45c1e3838582703505537de
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 21:13:42 -0500

    Add files via upload

---
+++ integers.py
@@ -0,0 +1,12 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+print(4 + 4)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
+
+
+fav_number = 24
+print("Kobe Bryant jersey number is " + str(fav_number) + ", and he is the GOAT!")
